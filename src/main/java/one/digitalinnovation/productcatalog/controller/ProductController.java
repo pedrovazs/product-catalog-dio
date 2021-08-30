4147a58fb89b07c3fb77fae3e2560933ce9a3811
Pedro Vaz <pedrovazs@outlook.com>
2021-08-30 14:49:44 -0300

    CRUD finished

--- src/main/java/one/digitalinnovation/productcatalog/controller/ProductController.java
+++ src/main/java/one/digitalinnovation/productcatalog/controller/ProductController.java
@@ -5,7 +5,9 @@ import one.digitalinnovation.productcatalog.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -17,22 +19,28 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @PostMapping
-    public Product create(@RequestBody Product produto) {
-        return productRepository.save(produto);
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
     }
 
     @PutMapping
-    public Product update(@RequestBody Product produto) {
-        return productRepository.save(produto);
+    public Product update(@RequestBody Product product) {
+        return productRepository.save(product);
     }
 
     @GetMapping
-    public Iterable<Product> findProduct() {
-        return productRepository.findAll();
+    public List<Product> findProduct() {
+        return (List<Product>) productRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Optional<Product> findById(@PathVariable Long id) {
         return productRepository.findById(id);
     }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
+
 }
